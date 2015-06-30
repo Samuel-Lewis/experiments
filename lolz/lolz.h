54241a483036328825dbabbca91471c60ff3d051
Samuel-Lewis <mazuel+github@gmail.com>
2015-07-01 00:31:16 +0930

    + Added a whole lot of new macros + Comments! - Removed the class declaration

--- lolz/lolz.h
+++ lolz/lolz.h
@@ -1,9 +1,52 @@
-
 #include <cstdlib>
 #include <cstdio>
 
-#define KTHXBYE std::exit(0);
 #define HIA int main()
+#define KTHXBYE std::exit(0);
 
-class lolz
-{};
+#define NUFFIN void
+#define DIS this
+#define PRETTEH new
+
+#define K ;
+#define EH ;
+
+#define COS_YEAH }
+#define BRACE_FOR_IMPACT {
+
+#define VISIBLE std::cout <<
+#define TLDR << std::endl;
+
+#define SPIT_OUT return
+#define BANISH break
+
+// COMMENTS
+#define BTW //
+#define STORY_TIMEZ /*
+#define THE_END */
+
+// CLASSES
+#define CLASSY class
+
+#define SECRETZ private
+#define SLUTTY public
+#define SORTA_HIDDEN protected
+
+// CONDITIONALS
+#define YA_RLY if 
+#define NO_WAI else
+
+#define IS ==
+#define NOT !=
+#define ITZ =
+#define BUT_MAYBE ||
+#define AND_DIS &&
+  
+#define WIN true
+#define FAIL false
+
+// OPERATORS
+#define SMAHED_WIF +
+#define WIF_OUT -
+#define SPLIT_WIF /
+#define TIMEZ_WIF *
